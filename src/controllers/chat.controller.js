0358c39e8e8a621ac4b96a1be2db7ce5394696be
Bishal Sutradhar <bishalsutradhar2006@gmail.com>
2026-07-24 02:53:47 +0530

    Add chat function in controller

---
+++ src/controllers/chat.controller.js
@@ -0,0 +1,16 @@
+const getMessage = require("../services/gemini")
+const chat = async (req, res) => {
+
+    const message = req.body.message
+    const response = await getMessage(message)
+
+    console.log(response)
+
+    res.status(200).json({
+        message: "Successfully received the AI response!",
+        response: response
+    })
+
+}
+
+module.exports = { chat }
